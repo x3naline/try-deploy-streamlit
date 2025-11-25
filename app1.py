71f64f0537217bb56b715ad49d55cc4a6d1a755a
x3naline <nabilacaca.101099@gmail.com>
2025-11-25 12:03:37 +0800

    delete 'plotly.express'

--- app1.py
+++ app1.py
@@ -1,7 +1,6 @@
 import streamlit as st
 import pandas as pd
 import matplotlib.pyplot as plt
-import plotly.express as px
 
 st.title("📊 Visualisasi Data Interaktif")
 st.write("Sistem ini menampilkan berbagai visualisasi dari dataset sederhana.")
